8a3446c2f2355a2d17872b7400fba5a8f6629c92
shij0d <jianwei.shi@kaust.edu.sa>
2024-10-08 10:01:18 +0300

    add results_eff

---
+++ expriements/decentralized/Unequal_sample_size/results_eff.py
@@ -0,0 +1,20 @@
+#%%
+import sys
+sys.path.append('/home/shij0d/documents/dis_LR_spatial')
+import pickle
+from src.utils import exact_parameters
+
+
+
+#%%
+
+with open(f'/home/shij0d/documents/dis_LR_spatial/expriements/decentralized/Unequal_sample_size/res.pkl','rb') as f:
+    results=pickle.load(f)
+    results_new=exact_parameters(results)
+
+with open(f'/home/shij0d/documents/dis_LR_spatial/expriements/decentralized/Unequal_sample_size/res_memeff.pkl', 'wb') as f:
+        pickle.dump(results_new, f)
+        
+        
+
+    
